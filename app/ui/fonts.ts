12567ee653c22f37194d384b5554d59d2e471fb0
petrodesignr <petrodesignr@gmail.com>
2024-06-10 15:28:48 +0200

    build fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-export const inter2 = Lusitana({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
